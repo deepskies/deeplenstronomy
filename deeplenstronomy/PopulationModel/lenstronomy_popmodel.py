13b8d88df698fcad413d8f8a5d8c0448d81c9a19
Brian Nord <nord@fnal.gov>
2019-02-20 13:04:23 -0600

    create population model template

---
+++ deeplenstronomy/PopulationModel/lenstronomy_popmodel.py
@@ -0,0 +1,74 @@
+
+
+class PopulationModel(object):
+    """
+    This class defines the population draws needed to produce a large set of lensing systems
+    """
+
+    def __init__(self):
+
+
+
+    def DrawHaloMass(self):
+        """
+        Defines mass function for galaxy-scale lenses
+        :return:
+        """
+        return halo_mass
+
+    def DrawApparentMagnitude(self):
+        """
+        Draws apparent magnitude from a distribution
+        :return:
+        """
+        return apparent_magnitude
+
+    def DrawAbsoluteMagnitude(self):
+        """
+        Draws absolute magnitude from a distribution
+        :return:
+        """
+        return absolute_magnitude
+
+    def DrawPositionAngle(self):
+        """
+        Draws position angle from a distribution
+        :return:
+        """
+        return position_angle
+
+    def DrawRedshift(selfs):
+        """
+        Draws position angle from a distribution
+        :return:
+        """
+        return redshift
+
+    def DrawEinsteinRadius(self):
+        """
+        Draw Einstein radius from a distribution
+        :return:
+        """
+        return einstein_radius
+
+    def DrawVelocityDispersion(self):
+        """
+        Draw Velocity Dispersion from a distribution
+        :return:
+        """
+        return velocity_dispersion
+
+    def DrawMagnification(self):
+        """
+        Draw magnification of source from a distribution
+        :return:
+        """
+        return magnification
+
+
+    def DrawCrosssection(self):
+        """
+        Draw magnification of source from a distribution
+        :return:
+        """
+        return cross_section
