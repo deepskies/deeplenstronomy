08852323a218f06995966b281e219f875c943f1f
joshualin24 <joshualin24@gmail.com>
2019-07-23 16:00:12 -0500

    added injection simulation and simple population models

---
+++ deeplenstronomy/ImSim/inject_simulations.py
@@ -0,0 +1,31 @@
+from lenstronomy.SimlualationAPI.sim_api import simAPI
+from lenstronomy.SimlualationAPI.observation_api import SingleBand
+from deeplenstronomy.PopSim.galaxy_prop import GalaxyProp
+
+
+
+def add_arc(image, kwargs_band, kwargs_source_mag, kwargs_lens, kwargs_model, kwargs_numerics=None):
+    """
+    routine to add lensed arc to existing image
+    :param image: 2d square numpy array of original image
+    :param kwargs_band: keyword arguments of noise and exposure properties of the image (see SimAPI for possible settings)
+    :param sourcePop: instance of SourcePop class
+    """
+    arc = _arc_model(kwargs_band, kwargs_source_mag, kwargs_lens, kwargs_model, kwargs_numerics)
+    band = SingleBand(**kwargs_band)
+    noisy_arc = arc + band.flux_noise(arc)
+    return image + noisy_arc
+
+
+def _arc_model(kwargs_band, kwargs_source_mag, kwargs_lens, kwargs_model, kwargs_numerics=None):
+    """
+    routine to simulate a lensing arc
+    TODO describe all parameters
+    """
+    simAPI = SimAPI(numpix=numpix, kwargs_single_band=kwargs_band, kwargs_model=kwargs_model, kwargs_numerics=kwargs_numerics)
+
+    imSim = simAPI.image_model_class
+    _, kwargs_source, _ = simAPI.magnitude2amplitude(kwargs_source_mag=kwargs_source_mag)
+    image = imSim.image(kwargs_lens, kwargs_source=kwargs_source)
+    noisy_image = image + simAPI.noise_for_model(model=image, background_noise=False, poisson_noise=True)
+    return noisy_image
--- deeplenstronomy/PopSim/galaxy_prop.py
+++ deeplenstronomy/PopSim/galaxy_prop.py
@@ -20,3 +20,34 @@ class GalaxyProp(object):
 
     def draw_ligth2_mass(self):
         pass
+
+
+class SourcePop():
+
+    def __int__(self):
+        pass
+
+    def draw_source_model():
+        """
+        draws source model from population
+        """
+        source_center_x = (np.random.rand() - 0.5) * 2
+        source_center_y = (np.random.rand() - 0.5) * 2
+        kwargs_source_mag = [{'magnitude': 22, 'R_sersic': 0.3, 'n_sersic': 1, 'e1': -0.3, 'e2': -0.2, 'center_x': source_center_x, 'center_y': source_center_y}]
+        source_model_list = ['SERSIC_ELLPISE']
+        return kwargs_source_mag, source_model_list
+
+    def draw_lens_model():
+        """
+        draw lens model parameters
+        return: lens model keyword argument list, lens model list
+        """
+        theta_E = np.random.uniform(0.9, 2.2)
+        lens_e1 = (np.random.rand() - 0.5) * 0.8
+        lens_e2 = (np.random.rand() - 0.5) * 0.8
+        kwargs_lens = [
+        {'theta_E': theta_E, 'e1': lens_e1, 'e2': lens_e2, 'center_x': 0, 'center_y': 0},  # SIE model
+        {'e1': 0.03, 'e2': 0.01}  # SHEAR model
+        ]
+        lens_model_list = ['SIE', 'SHEAR']
+        return kwargs_lens, lens_model_list
