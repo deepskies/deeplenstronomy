d8f799ba177aea81a9982e4c56a2b18b02d05465
sibirrer <sibirrer@gmail.com>
2019-07-24 09:42:40 -0500

    API for population module changed

--- deeplenstronomy/PopSim/galaxy_prop.py
+++ deeplenstronomy/PopSim/galaxy_prop.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 
-
 class SourcePop():
 
     def __int__(self):
@@ -32,9 +31,44 @@ class SourcePop():
         lens_model_list = ['SIE', 'SHEAR']
         return kwargs_lens, lens_model_list
 
-    def draw_model(self, **kwargs):
+    def draw_lens_light(self):
+        """
+
+        :return:
+        """
+        lens_light_model_list = ['SERSIC)_ELLIPSE']
+        kwargs_lens_light = [{'magnitude': 22, 'R_sersic': 0.3, 'n_sersic': 1, 'e1': -0.3, 'e2': -0.2, 'center_x': 0, 'center_y': 0}]
+        return kwargs_lens_light, lens_light_model_list
+
+    def draw_point_source(self, center_x, center_y):
+        """
+
+        :param center_x: center of point source in source plane
+        :param center_y: center of point source in source plane
+        :return:
+        """
+        point_source_model_list = ['SOURCE_POSITION']
+        kwargs_ps = [{'magnitude': 21, 'ra_source': center_x, 'dec_source': center_y}]
+        return kwargs_ps, point_source_model_list
+
+    def draw_model(self, with_lens_light=False, with_quasar=False, **kwargs):
         """
 
         :param kwargs:
         :return: kwargs_params, kwargs_model
         """
+        kwargs_lens, lens_model_list = self.draw_lens_model()
+        kwargs_params = {'kwargs_lens': kwargs_lens}
+        kwargs_model = {'lens_model_list': lens_model_list}
+        kwargs_source, source_model_list = self.draw_source_model()
+        kwargs_params['kwargs_source'] = kwargs_source
+        kwargs_model['source_model_list'] = source_model_list
+        if with_lens_light:
+            kwargs_lens_light, lens_light_model_list = self.draw_lens_light()
+            kwargs_params['kwargs_lens_light'] = kwargs_lens_light
+            kwargs_model['lens_light_model_list'] = lens_light_model_list
+        if with_quasar:
+            kwargs_ps, point_source_model_list = self.draw_point_source(center_x=kwargs_source[0]['center_x'], center_y=kwargs_source[0]['center_y'])
+            kwargs_params['kwargs_ps'] = kwargs_ps
+            kwargs_model['point_source_model_list'] = point_source_model_list
+        return kwargs_params, kwargs_model
