313d9d2a78f6af8df94e8c760f2b1aa759cab22c
Brian Nord <nord@fnal.gov>
2019-07-25 11:19:16 -0500

    finished draw_model_physical

--- test/test_PopSim/test_population.py
+++ test/test_PopSim/test_population.py
@@ -13,6 +13,11 @@ class TestPopulation(object):
         kwargs_params, kwargs_model = pop.draw_model(with_lens_light=True, with_quasar=True)
         assert kwargs_model['lens_model_list'][0] == 'SIE'
 
+    def test_draw_physical_model(self):
+
+        pop = Population()
+        kwargs_params, kwargs_model = pop.draw_model(with_lens_light=True, with_quasar=True, mode='complex')
+        assert kwargs_model['lens_model_list'][0] == 'SIE'
 
 if __name__ == '__main__':
     pytest.main()
