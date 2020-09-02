9500dacbd36346295c142769eb270bb0fec828c1
Robert Morgan <robert.morgan@wisc.edu>
2020-09-02 09:52:22 -0500

    add delve mode

--- deeplenstronomy/surveys.py
+++ deeplenstronomy/surveys.py
@@ -34,3 +34,35 @@ SURVEY:
                 PARAMETERS: None
 """
     return info
+
+def delve():
+    info = """
+IMAGE:
+    PARAMETERS:
+        exposure_time:
+            DISTRIBUTION:
+                NAME: delve_exposure_time
+                PARAMETERS: None
+        numPix: 100
+        pixel_scale: 0.263
+        psf_type: 'GAUSSIAN'
+        read_noise: 7
+        ccd_gain:
+            DISTRIBUTION:
+                NAME: des_ccd_gain
+                PARAMETERS: None
+SURVEY:
+    PARAMETERS:
+        BANDS: g,r,i,z
+        seeing:
+            DISTRIBUTION:
+                NAME: delve_seeing
+                PARAMETERS: None
+        magnitude_zero_point: 30.0
+        sky_brightness:
+            DISTRIBUTION:
+                NAME: delve_sky_brightness
+                PARAMETERS: None
+        num_exposures: 1
+"""
+    return info
