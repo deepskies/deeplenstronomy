620a02b9b2ad186fea5a506a97595e89d09a9bd8
Robert Morgan <robert.morgan@wisc.edu>
2020-11-09 11:48:34 -0600

    include requirements in setup

--- setup.py
+++ setup.py
@@ -22,11 +22,14 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open(path.join(this_directory, 'requirements.txt'), encoding='utf-8') as req:
+    requirements = [x.strip() for x in req.readlines()]
+
 
 
 setup(
     name='deeplenstronomy',
-    version='0.0.0.12',
+    version='0.0.0.14',
     description='wrap lenstronomy for efficient simulation generation',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -38,18 +41,17 @@ setup(
     ],
     package_dir={'deeplenstronomy': 'deeplenstronomy'},
     include_package_data=True,
-    install_requires=[
-    ],
+    install_requires=requirements,
     license='MIT',
     zip_safe=False,
     keywords='deeplenstronomy',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable'
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 )
