3c559690f1b6b8a3a2758180acf5471759e8d670
Robert Morgan <robert.morgan@wisc.edu>
2021-05-07 13:42:59 -0500

    increment to stable 0.0.2.0

--- deeplenstronomy/__init__.py
+++ deeplenstronomy/__init__.py
@@ -1,5 +1,6 @@
 """Welcome to deeplenstronomy! Check out example notebooks here: https://deepskies.github.io/deeplenstronomy"""
 
+
 __author__ = 'Robert Morgan'
 __email__ = 'robert.morgan@wisc.edu'
-__version__ = '0.0.1.9'
+__version__ = '0.0.2.0'
