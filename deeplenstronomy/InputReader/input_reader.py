7cd0e646bd79da304311c996a26978525ef88cc2
voetberg <magpie127@gmail.com>
2023-02-23 09:25:07 -0600

    Class with methods and most inputs.

--- deeplenstronomy/InputReader/input_reader.py
+++ deeplenstronomy/InputReader/input_reader.py
@@ -2,8 +2,11 @@ from abc import ABC, abstractmethod
 
 
 class InputReader(ABC):
-    def __init__(self, input_source: str=None):
+    def __init__(self, input_source: str = None):
         self.dataset_parameters = self.read_input(input_source)
+
+        # Break that dictionary into segments that can be used by the input reader
+        # Without parsing through the whole dic every time a parameters is set
 
     def read_input(self, input_source):
         # Opens and validates and sets all the params given by the input
@@ -24,25 +27,39 @@ class InputReader(ABC):
 
     # Methods to set each param individually
     # Making using the input_source optional
-    def survey(self, **kwargs):
+    def bands(self, bands: list = None):
+        # Reset bands that will be used to format the dictionary
         pass
 
-    def objects(self):
+    def survey(self, survey=None, **kwargs):
         pass
 
-    def cosmology(self):
+    def objects(self, object_id=None, **kwargs):
         pass
 
-    def image(self):
+    # Timeseries Input (pointings added) a subclass of the I/O
+
+    def cosmology(self, *kwargs):
+        # I don't know what sort of things are allowed for cosmology
+        # TODO Consult Brian or docs on cosmo parameters
         pass
 
-    def noise(self):
+    def image(self, **kwargs):
         pass
 
-    def geometry(self):
+    def noise(self, number_noise_sources=None, noise_source=None, **kwargs):
         pass
 
-    def species(self):
+    def geometry(self,
+                 plane=None,
+                 red_shift=None,
+                 **kwargs):
+        # Sets the parameters for a specific plane
+        pass
+
+    def species(self, plane=None, species_name=None, profile=None):
+        # Adds a species to a particular plane
+        # Uses the _species_* helper functions to set them based on the passed profile
         pass
 
     def _species_light_profiles(self):
@@ -57,5 +74,16 @@ class InputReader(ABC):
     def _species_special(self):
         pass
 
-    def save(self, output_path):
+    def _format_dict(self):
+        # Add all these parameters to the dictionary, used when calling the reader
+        # Will work with all these default ones,
+        # and let it be extended into different types of survey
         pass
+
+    def save(self, output_path):
+        # Save results as {preformed config that can be re-run by feeding it to the io}
+        pass
+
+    def __call__(self, *args, **kwargs):
+        # Put this whole thing together into the dictionary that can be used by lenstronomy
+        return self._format_dict()
